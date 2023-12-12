a09b56798c06e28da681b3bbac3a78f23c059304
Bhanu <7218584+bhanucb@users.noreply.github.com>
2023-12-11 23:20:09 -0500

    feat: add grid in home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,14 +3,29 @@ import PriceChangesGrid from "./PriceChangesGrid";
 import { styled } from "@mui/material/styles";
 import { Paper } from "@mui/material";
 import { NAVIGATION_BAR_HEIGHT } from "../../navigation/Constants";
+import Grid from "@mui/material/Unstable_Grid2";
 
 const Page = styled(Paper)`
-  height: calc(100vh - ${NAVIGATION_BAR_HEIGHT}px);
+  height: calc(100vh - (${NAVIGATION_BAR_HEIGHT}px));
+  border-radius: 0;
 `;
 const Home: FC = () => {
   return (
     <Page>
-      <PriceChangesGrid />
+      <Grid container style={{ height: "100%" }}>
+        <Grid xs={8} p={1}>
+          <PriceChangesGrid />
+        </Grid>
+        <Grid xs={4} p={1}>
+          <PriceChangesGrid />
+        </Grid>
+        <Grid xs={8} p={1}>
+          <PriceChangesGrid />
+        </Grid>
+        <Grid xs={4} p={1}>
+          <PriceChangesGrid />
+        </Grid>
+      </Grid>
     </Page>
   );
 };

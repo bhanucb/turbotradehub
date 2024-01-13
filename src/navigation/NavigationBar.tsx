73224683ffd096e4ad0e2d036b017c028684df07
Bhanu <7218584+bhanucb@users.noreply.github.com>
2024-01-13 13:24:59 -0500

    Implement dynamic AppBar color in NavigationBar
    The NavigationBar component now interprets the currently active theme and dynamically changes the AppBar color accordingly. This provides a better user interface experience catering to the selected dark or light mode settings.

--- src/navigation/NavigationBar.tsx
+++ src/navigation/NavigationBar.tsx
@@ -2,10 +2,12 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { ThemeSection } from "./ThemeSection";
+import {useTheme} from "@mui/material";
 
 function NavigationBar() {
+  const {palette: {mode}} = useTheme();
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{bgcolor: mode === "light" ? "#283593" : "primary"}}>
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Turbo Trade Hub
